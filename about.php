233b9365c2a216e39af52ebc25911b7e47da4815
Mahendra-uk <support@techimo.in>
2025-07-11 11:28:49 +0530

    about page template created

---
+++ about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>iDiscuss Forum - About</title>
+    <?php require 'partials/_bootstrap.php'; ?>
+    <?php require 'partials/_dbconnect.php'; ?>
+  </head>
+  <body>
+    <section class="main-section">
+      <!-- Header -->
+      <?php require 'partials/_header.php'; ?>
+      <div class="container my-5">
+        <h1 class="text-center display-3">About Us</h1>
+        <p class="lead text-center mb-5">Building a vibrant community where developers and tech enthusiasts learn, collaborate, and innovate together every day.</p>
+       
+      </div>
+    </section>
+    <!-- Footer -->
+    <?php require 'partials/_footer.php'; ?>
+  </body>
+</html>
